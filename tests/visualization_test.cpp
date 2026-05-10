c1b1af37b803fbf4bc441bfa30f460fb6a69b341
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-05-10 16:27:11 +0300

    add: добавил unit тесты для задания

--- tests/visualization_test.cpp
+++ tests/visualization_test.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
-#include "visualization.hpp"
 
-TEST(VisualizationTest, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+TEST(VisualizationTest, SimpleCheck) 
+{
+    EXPECT_EQ(1 + 1, 2);
+}
